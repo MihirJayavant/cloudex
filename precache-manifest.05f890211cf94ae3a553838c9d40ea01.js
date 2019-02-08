e080d31743d58865d92e0100f03c81ea5ecdea3b
mihirjayavant <mihir.jayavant2@gmail.com>
2019-02-08 15:07:16 +0530

    Updates

---
+++ precache-manifest.05f890211cf94ae3a553838c9d40ea01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d27c321de604519248",
+    "url": "/react-demo/static/js/runtime~main.d6d27c32.js"
+  },
+  {
+    "revision": "916d4560808cf95c56c9",
+    "url": "/react-demo/static/js/main.916d4560.chunk.js"
+  },
+  {
+    "revision": "1fa1b67c9b48a81d4cb3",
+    "url": "/react-demo/static/js/1.1fa1b67c.chunk.js"
+  },
+  {
+    "revision": "916d4560808cf95c56c9",
+    "url": "/react-demo/static/css/main.8d4e0ecd.chunk.css"
+  },
+  {
+    "revision": "84e6f2b482566bcc940cb34d87856c1f",
+    "url": "/react-demo/index.html"
+  }
+];
