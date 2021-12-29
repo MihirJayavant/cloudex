b97a3ddb322946803ee9ad17ce54294384e0ee44
mihirjayavant <mihir.jayavant2@gmail.com>
2021-12-29 17:08:39 +0530

    tabs added

--- src/containers/home-page/home-page.tsx
+++ src/containers/home-page/home-page.tsx
@@ -1,12 +1,12 @@
-import { Box, Heading, Text, Flex, Grid, Image } from '@chakra-ui/react'
+import { Box, Heading, Text, Flex, Grid, Image, Tabs, TabList, Tab, TabPanels, TabPanel } from '@chakra-ui/react'
 import * as React from 'react'
 import { useNavigate } from 'react-router-dom'
 import ReactLogo from '../../assets/ReactLogo.png'
-import AngularLogo from '../../assets/AngularLogo.png';
+import AngularLogo from '../../assets/AngularLogo.png'
 
 function HomePage() {
   const navigate = useNavigate()
-  function gotoDockerPage (application: string) {
+  function gotoDockerPage(application: string) {
     navigate(`/docker/${application}`)
   }
 
@@ -14,42 +14,70 @@ function HomePage() {
     <Flex direction="column">
       <Box display="flex" flexDirection="column" justifyContent="center" alignItems="flex-start" h="30%" py={8} px={10} backgroundColor="blue.400">
         <Heading mb={4}>Cloudex</Heading>
-        <Text fontSize='xl'>
-          Containerized your Application
-        </Text>
+        <Text fontSize="xl">Containerized your Application</Text>
       </Box>
-      <Grid templateColumns={{ base: 'repeat(1, 1fr)', lg: 'repeat(5, 1fr)', md: 'repeat(3, 1fr)' }} gap={6} py={8} px={10}>
-        <Box style={{ cursor: "pointer", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }} maxW="sm" borderWidth='1px' borderRadius='lg' onClick={() => gotoDockerPage('angular')}>
-            <Image src={AngularLogo} alt="Angular" height="150px" objectFit='cover' />
-            <Box p='6'>
+      <Tabs align="center" variant="solid-rounded" mt={5}>
+        <TabList>
+          <Tab>Docker</Tab>
+          <Tab>Kubernetes</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <Grid
+              templateColumns={{
+                base: 'repeat(1, 1fr)',
+                lg: 'repeat(5, 1fr)',
+                md: 'repeat(3, 1fr)',
+              }}
+              gap={6}
+              py={8}
+              px={10}
+            >
               <Box
-                textAlign="center"
-                mt='1'
-                fontWeight='semibold'
-                as='h4'
-                lineHeight='tight'
-                isTruncated
+                style={{
+                  cursor: 'pointer',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+                maxW="sm"
+                borderWidth="1px"
+                borderRadius="lg"
+                onClick={() => gotoDockerPage('angular')}
               >
-                Angular
+                <Image src={AngularLogo} alt="Angular" height="150px" objectFit="cover" />
+                <Box p="6">
+                  <Box textAlign="center" mt="1" fontWeight="semibold" as="h4" lineHeight="tight" isTruncated>
+                    Angular
+                  </Box>
+                </Box>
               </Box>
-            </Box>
-          </Box>
-          <Box style={{ cursor: "pointer", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }} maxW="sm" borderWidth='1px' borderRadius='lg' onClick={() => gotoDockerPage('react')}>
-            <Image src={ReactLogo} alt="React" height="150px" objectFit='cover' />
-            <Box p='6'>
               <Box
-                textAlign="center"
-                mt='1'
-                fontWeight='semibold'
-                as='h4'
-                lineHeight='tight'
-                isTruncated
+                style={{
+                  cursor: 'pointer',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+                maxW="sm"
+                borderWidth="1px"
+                borderRadius="lg"
+                onClick={() => gotoDockerPage('react')}
               >
-                React
+                <Image src={ReactLogo} alt="React" height="150px" objectFit="cover" />
+                <Box p="6">
+                  <Box textAlign="center" mt="1" fontWeight="semibold" as="h4" lineHeight="tight" isTruncated>
+                    React
+                  </Box>
+                </Box>
               </Box>
-            </Box>
-          </Box>
-      </Grid>
+            </Grid>
+          </TabPanel>
+          <TabPanel>Kubs</TabPanel>
+        </TabPanels>
+      </Tabs>
     </Flex>
   )
 }
