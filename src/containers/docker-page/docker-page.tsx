93aeeffc076a88ebd23d3c7a5894d70c2485a94a
mihir jayavant <mihir.jayavant2@gmail.com>
2022-01-02 17:12:09 +0530

    docker compose done

--- src/containers/docker-page/docker-page.tsx
+++ src/containers/docker-page/docker-page.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Box, Flex, FormControl, FormLabel, Select, Button, Container } from '@chakra-ui/react'
+import { Box, Flex, FormControl, FormLabel, Select, Button, RadioGroup, Stack, Radio, Switch } from '@chakra-ui/react'
 import { AngularBuilder, AngularComposeBuilder } from '../../core/docker'
 // import { useParams } from 'react-router-dom'
 import { Header } from '../../components/Header'
@@ -12,9 +12,21 @@ function DockerPage() {
   const [file, setFile] = React.useState<string[]>([])
   const [compose, setCompose] = React.useState<string[]>([])
 
-  const generateDockerfile = () => {
-    setFile(new AngularBuilder().build())
-    setCompose(new AngularComposeBuilder().build())
+  const generateDockerfile = async () => {
+    const w = window as any
+    const dir = await w.showDirectoryPicker()
+    const file = await dir.getFileHandle('dockerfile', { create: true })
+    const file2 = await dir.getFileHandle('docker-compose.yml', { create: true })
+    const docker = new AngularBuilder().build()
+    const compose = new AngularComposeBuilder().build()
+    setFile(docker)
+    setCompose(compose)
+    const writable = await file.createWritable()
+    await writable.write(docker.join('\n'))
+    await writable.close()
+    const writable2 = await file2.createWritable()
+    await writable2.write(compose.join('\n'))
+    await writable2.close()
     setTimeout(() => hljs.highlightAll(), 0)
   }
 
@@ -30,41 +42,48 @@ function DockerPage() {
     <div>
       <Header />
 
-      <Container>
-        <Flex direction="column" justifyContent="space-between">
-          <Flex direction="column" justifyContent="space-between">
-            <FormControl>
-              <FormLabel htmlFor="nodeVersion">Node Version</FormLabel>
-              <Select id="nodeVersion" placeholder="Select Version">
-                <option>Node 16</option>
-                <option>Node 14</option>
-              </Select>
-            </FormControl>
-            <FormControl>
-              <FormLabel htmlFor="packageRun">Package</FormLabel>
-              <Select id="packageRun" placeholder="Select package">
-                <option>Yarn</option>
-                <option>Npm</option>
-              </Select>
-            </FormControl>
-            <FormControl mt={5}>
-              <Button colorScheme="teal" size="md" onClick={generateDockerfile}>
-                Generate
-              </Button>
-            </FormControl>
-          </Flex>
-          <Box mt={5} mb={5}>
-            <pre>
-              <code className="language-dockerfile">{lines(file)}</code>
-            </pre>
-          </Box>
-          <Box mt={5} mb={5}>
-            <pre>
-              <code className="language-yml">{lines(compose)}</code>
-            </pre>
-          </Box>
+      <Flex direction="column" justifyContent="space-between">
+        <Flex direction="column" justifyContent="space-between" maxWidth={500}>
+          <FormControl display="flex" alignItems="center" margin={5}>
+            <FormLabel htmlFor="nodeVersion">Node Version</FormLabel>
+            <Select id="nodeVersion" defaultValue={16}>
+              <option value={16}>v16</option>
+              <option value={14}>v14</option>
+            </Select>
+          </FormControl>
+          <FormControl display="flex" alignItems="center" margin={5}>
+            <RadioGroup defaultValue="npm">
+              <Stack spacing={4} direction="row">
+                <Radio value="npm" isDisabled>
+                  npm
+                </Radio>
+                <Radio value="yarn">yarn</Radio>
+              </Stack>
+            </RadioGroup>
+          </FormControl>
+          <FormControl display="flex" alignItems="center" margin={5}>
+            <FormLabel htmlFor="ssr-docker-id" mb="0">
+              Server side rendering enabled?
+            </FormLabel>
+            <Switch id="ssr-docker-id" />
+          </FormControl>
+          <FormControl margin={5}>
+            <Button colorScheme="teal" size="md" onClick={generateDockerfile}>
+              Generate
+            </Button>
+          </FormControl>
         </Flex>
-      </Container>
+        <Box mt={5} mb={5}>
+          <pre>
+            <code className="language-dockerfile">{lines(file)}</code>
+          </pre>
+        </Box>
+        <Box mt={5} mb={5}>
+          <pre>
+            <code className="language-yml">{lines(compose)}</code>
+          </pre>
+        </Box>
+      </Flex>
     </div>
   )
 }
